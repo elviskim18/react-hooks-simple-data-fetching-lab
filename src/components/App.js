9111f63d9d77e9c8f56694e726d1312c726886f3
Kimani <elvis.kimani@student.moringaschool.com>
2022-07-18 14:58:45 +0300

    commit tree d7efb5219c399185ea1f85d95db2b0d42b2929dd

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+ 
+import React, {useEffect, useState} from "react";
+
+function App(){
+    const [dogImage, setdoImage] = useState([]);
+    const [status, setstatus] = useState(false);
+
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) =>{
+            setdoImage(data.message)
+            setstatus(true)
+        })
+
+        
+
+
+    }, []);
+    if(!status)
+         return <p>Loading...</p>
+
+    return (
+        <img src={dogImage} alt="A Random Dog"></img>
+    )
+}
+
+export default App
